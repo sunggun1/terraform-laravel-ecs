5d0fb05ba462fddee03c8b02b1ef931b102983b7
sunggun1 <readingdeveloper@naver.com>
2021-10-15 19:22:53 +0900

    start_script.sh

---
+++ laravelApp/start_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -m
+
+#turn on bash's job control
+#Start the primary process and put it in the background
+php-fpm -y /usr/local/etc/php-fpm.conf -R &
+#Start the helper process
+php artisan migrate
+
+#the my_helper_process might need to know how to wait on the
+#primary process to start before it does its work and returns
+
+#now we bring the primary process back into the foreground 
+# and leave it there
+fg %1
